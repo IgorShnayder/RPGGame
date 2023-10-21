ebba2dc60fbfcbaf1a69da211bfb195e1e11ac0d
Igor Shnayder <shnayder.ig@gmail.com>
2023-10-21 17:24:52 +0200

    update

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -27,20 +27,16 @@ public class Bridge : MonoBehaviour
 
     private void Break()
     {
-        // Вырезаем отверстие в навмеш (чтобы игрок там больше не смог пройти)
         _navMeshObstacle.enabled = true;
         
         foreach (var rigidbody in _rigidbodies)
         {
             rigidbody.isKinematic = false;
-
-            // Генерируем случайную силу.
+            
             var forceValue = Random.Range(_minForceValue, _maxForceValue);
-
-            // Генерируем случайное направление.
+            
             var direction = Random.insideUnitSphere;
             
-            // Придаем силу каждому rigidbody.
             rigidbody.AddForce(direction * forceValue);
         }
     }
